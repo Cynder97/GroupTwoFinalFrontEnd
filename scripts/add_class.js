21b27760bd6bce369830bc5a5643131d2f3bad87
Cynder97 <150056594+Cynder97@users.noreply.github.com>
2025-05-07 11:58:01 -0400

    Update add_class.js

--- scripts/add_class.js
+++ scripts/add_class.js
@@ -12,7 +12,7 @@ async function addSong(){
         description: document.querySelector("#description").value
     }
 
-    const response = await fetch("https://therapeutic-roan-raven.glitch.me", {
+    const response = await fetch("https://therapeutic-roan-raven.glitch.me/api/courses", {
         method: "POST",
         headers:{
             "Content-Type" : "application/json"
